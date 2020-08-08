ac1c2fd9b6340437ecf7c5ca251ee79204843b06
Rodrigo Celvo <rodrigo.pc.12@gmail.com>
2020-08-07 23:54:40 -0300

    Challenge finalized

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -1,11 +1,20 @@
+import { container } from 'tsyringe';
+
 import { Request, Response } from 'express';
 
 import CreateCustomerService from '@modules/customers/services/CreateCustomerService';
 
-import { container } from 'tsyringe';
-
 export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, email } = request.body;
+
+    const createCustomer = container.resolve(CreateCustomerService);
+
+    const customer = await createCustomer.execute({
+      name,
+      email,
+    });
+
+    return response.json(customer);
   }
 }
